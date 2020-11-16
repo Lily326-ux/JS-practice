a1b45f14bde92abe329a27eae695f6274cc1450c
Lily326-ux <uprt.lily228@gmail.com>
2020-11-16 20:12:05 +0200

    5 tasks

---
+++ js-pracrice-2/index.js
@@ -0,0 +1,73 @@
+//The First Task
+
+var secondsInAMinute = 60;
+var minutesInAnHour = 60;
+var secondsInAHours = secondsInAMinute * minutesInAnHour;
+console.log(secondsInAHours);
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAHours * hoursInADay;
+console.log(secondsInADay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear);
+
+var myAge = 19;
+var secondsInMyLife = myAge * secondsInAYear;
+console.log(secondsInMyLife);
+
+
+//The Second Task
+
+var number1 = 7;
+var number2 = 8;
+var theResultOfNumbers = number1 + number2;
+console.log(theResultOfNumbers);
+
+var theResultOfNumbers2 = theResultOfNumbers - number2;
+console.log(theResultOfNumbers2);
+
+var theResultOfNumbers3 = theResultOfNumbers * theResultOfNumbers2;
+console.log(theResultOfNumbers3);
+
+var theResultOfNumbers4 = theResultOfNumbers3 / theResultOfNumbers2;
+console.log(theResultOfNumbers4);
+
+console.log(theResultOfNumbers4 ** 2);
+
+
+//The Third Task 
+
+var numberNull = 0;
+console.log(++numberNull);
+console.log(++numberNull);
+console.log(++numberNull);
+console.log(numberNull++);
+console.log(numberNull++);
+console.log(numberNull++);
+
+console.log(--numberNull);
+console.log(--numberNull);
+console.log(--numberNull);
+console.log(numberNull--);
+console.log(numberNull--);
+
+//The Fourth Task
+
+var number1 = 15;
+console.log(number1 += 22);
+
+var number2 = 20;
+console.log(number2 -= 16);
+
+//The Fifth Task
+
+var firstString = "Hello ";
+var secondString = "my friend!";
+console.log(firstString + secondString);
+
+var myName = "Lily ";
+var mySurname = "Omelchuk,";
+var myAge = " 19 years old.";
+console.log(myName + mySurname + myAge);
