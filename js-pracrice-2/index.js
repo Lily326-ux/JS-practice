420745dccca408143b172d295b4b19d9cc739591
Lily326-ux <uprt.lily228@gmail.com>
2020-11-28 14:46:49 +0200

    new task

--- js-pracrice-2/index.js
+++ js-pracrice-2/index.js
@@ -1,102 +1,196 @@
-//The First Task
+// //The First Task
 
-var secondsInAMinute = 60;
-var minutesInAnHour = 60;
-var secondsInAHours = secondsInAMinute * minutesInAnHour;
-console.log(secondsInAHours);
+// var secondsInAMinute = 60;
+// var minutesInAnHour = 60;
+// var secondsInAHours = secondsInAMinute * minutesInAnHour;
+// console.log(secondsInAHours);
 
-var hoursInADay = 24;
-var secondsInADay = secondsInAHours * hoursInADay;
-console.log(secondsInADay);
+// var hoursInADay = 24;
+// var secondsInADay = secondsInAHours * hoursInADay;
+// console.log(secondsInADay);
 
-var daysInAYear = 365;
-var secondsInAYear = secondsInADay * daysInAYear;
-console.log(secondsInAYear);
+// var daysInAYear = 365;
+// var secondsInAYear = secondsInADay * daysInAYear;
+// console.log(secondsInAYear);
 
-var myAge = 19;
-var secondsInMyLife = myAge * secondsInAYear;
-console.log(secondsInMyLife);
-
-
-//The Second Task
-
-var number1 = 7;
-var number2 = 8;
-var theResultOfNumbers = number1 + number2;
-console.log(theResultOfNumbers);
-
-var theResultOfNumbers2 = theResultOfNumbers - number2;
-console.log(theResultOfNumbers2);
-
-var theResultOfNumbers3 = theResultOfNumbers * theResultOfNumbers2;
-console.log(theResultOfNumbers3);
-
-var theResultOfNumbers4 = theResultOfNumbers3 / theResultOfNumbers2;
-console.log(theResultOfNumbers4);
-
-console.log(theResultOfNumbers4 ** 2);
+// var myAge = 19;
+// var secondsInMyLife = myAge * secondsInAYear;
+// console.log(secondsInMyLife);
 
 
-//The Third Task 
+// //The Second Task
 
-var numberNull = 0;
-console.log(++numberNull);
-console.log(++numberNull);
-console.log(++numberNull);
-console.log(numberNull++);
-console.log(numberNull++);
-console.log(numberNull++);
+// var number1 = 7;
+// var number2 = 8;
+// var theResultOfNumbers = number1 + number2;
+// console.log(theResultOfNumbers);
 
-console.log(--numberNull);
-console.log(--numberNull);
-console.log(--numberNull);
-console.log(numberNull--);
-console.log(numberNull--);
+// var theResultOfNumbers2 = theResultOfNumbers - number2;
+// console.log(theResultOfNumbers2);
 
-//The Fourth Task
+// var theResultOfNumbers3 = theResultOfNumbers * theResultOfNumbers2;
+// console.log(theResultOfNumbers3);
 
-var number1 = 15;
-console.log(number1 += 22);
+// var theResultOfNumbers4 = theResultOfNumbers3 / theResultOfNumbers2;
+// console.log(theResultOfNumbers4);
 
-var number2 = 20;
-console.log(number2 -= 16);
+// console.log(theResultOfNumbers4 ** 2);
 
-//The Fifth Task
 
-var firstString = "Hello ";
-var secondString = "my friend!";
-console.log(firstString + secondString);
+// //The Third Task 
 
-var myName = "Lily ";
-var mySurname = "Omelchuk,";
-var myAge = " 19 years old.";
-console.log(myName + mySurname + myAge);
+// var numberNull = 0;
+// console.log(++numberNull);
+// console.log(++numberNull);
+// console.log(++numberNull);
+// console.log(numberNull++);
+// console.log(numberNull++);
+// console.log(numberNull++);
 
-//The Sixth Task 
+// console.log(--numberNull);
+// console.log(--numberNull);
+// console.log(--numberNull);
+// console.log(numberNull--);
+// console.log(numberNull--);
 
-var stringFive = 5;
-var numberSix  = "6";
-console.log(stringFive + numberSix);
+// //The Fourth Task
 
-var num = 19;
-var stringAge = " years old."
-console.log(num + stringAge); 
+// var number1 = 15;
+// console.log(number1 += 22);
 
-//The Seventh Task 
+// var number2 = 20;
+// console.log(number2 -= 16);
 
-var stringNum = Number("7");
-var num = 11;
-console.log(stringNum + num);
+// //The Fifth Task
 
-//The Eighth Task 
+// var firstString = "Hello ";
+// var secondString = "my friend!";
+// console.log(firstString + secondString);
 
-var stringGreeting = "Hello ";
-var stringNotANumber = Number("world!");
-console.log(stringGreeting + stringNotANumber);
-/*isNaN(Number = stringNotANumber);
-console.log*/
+// var myName = "Lily ";
+// var mySurname = "Omelchuk,";
+// var myAge = " 19 years old.";
+// console.log(myName + mySurname + myAge);
 
-//The Ninth Task 
+// //The Sixth Task 
 
-var aboutMe = "My name is Lily";
-console.log(aboutMe.length);
+// var stringFive = 5;
+// var numberSix  = "6";
+// console.log(stringFive + numberSix);
+
+// var num = 19;
+// var stringAge = " years old."
+// console.log(num + stringAge); 
+
+// //The Seventh Task 
+
+// var stringNum = Number("7");
+// var num = 11;
+// console.log(stringNum + num);
+
+// //The Eighth Task 
+
+// var stringGreeting = "Hello ";
+// var stringNotANumber = Number("world!");
+// console.log(stringGreeting + stringNotANumber);
+
+// //The Ninth Task 
+
+// var aboutMe = "My name is Lily";
+// console.log(aboutMe.length);
+
+// //The Tenth Task
+
+// var apple = true;
+// var milk = false;
+// console.log(milk);
+// console.log(apple);
+
+// /*
+// //Calc
+ 
+// var num = Number(prompt("enter first number"));
+// var num2 = Number(prompt("enter second number"));
+
+// if (isNaN(num) && isNaN(num2)) {
+//   console.log("incorrect first value or second value"); 
+// }
+// else if(num === 0 || num2 === 0) {
+//   console.log("incorrect operation");
+// }
+// else {
+//   var sum = num / num2;
+//   console.log(sum);
+// }
+
+// var string = "hello";
+// var num = "2";
+//  console.log(isNaN((Number(num))));
+
+//  var num1 = 4;
+//  var num2 = "4";
+//  console.log(num1 == num2);
+
+// var age = Number(prompt("enter your age", 18));
+// var height = Number(prompt("enter your height", 190));
+// var gender = prompt("enter your gender", "man / woman");
+
+// if(gender === "man") {
+//   console.log((height - 100) * 1.15);
+// }
+// else if(gender === "woman") {
+//   console.log((height - 110) * 1.15);
+// }
+// else {
+//   console.log("i don't know" + " " + gender);
+// }
+// */
+
+// var myTopThreeMeals = ["Meat", "Chocolate", "Coffee"];
+// console.log(myTopThreeMeals.length);
+// console.log(myTopThreeMeals);
+
+// console.log(myTopThreeMeals[0]);
+
+// console.log(myTopThreeMeals[2]);
+
+// myTopThreeMeals[0] = "Fish";
+// console.log(myTopThreeMeals);
+
+// myTopThreeMeals[15] = "Milk";
+// console.log(myTopThreeMeals);
+
+// var myTopThreeMeals1 = ["Meat",["Milk", "Fish"], "Chocolate", "Coffee"];
+// console.log(myTopThreeMeals1[1][0]);
+
+// console.log(myTopThreeMeals1.length);
+
+// var toys = ["ball", "doll", "puzzlecondition"];
+// console.log(toys[toys.length -1]);
+
+//First
+
+// var firstNum = prompt("enter your first number");
+// var secondNum = prompt("enter your second number");
+
+// if(firstNum < secondNum){
+//   console.log(secondNum);
+// }
+
+// else if(firstNum > secondNum){
+//   console.log(firstNum);
+// }
+
+//Second
+
+var firstNum = Number;
+var secondNum = Number;
+var thirdNum = Number;
+
+if(firstNum === secondNum === thirdNum){
+  console.log(Number < Number);
+}
+
+else if(firstNum ){
+
+}
